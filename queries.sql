8ddb4411c7075b928e260e9874ddd35159678d58
Rosalyn <rosalyn.stevenor@gmail.com>
2022-02-27 13:12:21 -0600

    Configured and add sql database (running locally) to application.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 -- Part 1: Test it with SQL
+CREATE TABLE `job` (
+  `id` int NOT NULL,
+  `employer` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `skills` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 -- Part 2: Test it with SQL
 
