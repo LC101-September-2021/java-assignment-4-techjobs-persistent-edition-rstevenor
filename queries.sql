2284525544dec1b27feff1381f5af1e9e7d2a3cd
Rosalyn <rosalyn.stevenor@gmail.com>
2022-03-06 22:35:20 -0600

    Part Two completed. Add EmployerRepository and SkillRepository with necessary adjustments to controllers and the AbstractEntity

--- queries.sql
+++ queries.sql
@@ -5,9 +5,10 @@ CREATE TABLE `job` (
   `name` varchar(255) DEFAULT NULL,
   `skills` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location may not be blank")
+    @Size(min=3, max=200, message="Location must be between 3 and 200 characters long")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(String location){
+        this.location=location;
+    }
+
+    public Employer(){}
 }
